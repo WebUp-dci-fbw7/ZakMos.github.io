72fcbb35a1935c67fe7edbe42603a8c592a797ec
ZakMos <z.zza@hotmail.com>
2020-02-13 22:41:35 +0100

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,60 +1,44 @@
 import React, {  Fragment } from 'react';
-import { NavLink as RRNavLink } from 'react-router-dom';
-import { Nav, NavItem, NavLink, DropdownItem, DropdownToggle, Dropdown, DropdownMenu } from 'reactstrap';
-
-import projects from '../projects.json'
+import { Nav, NavItem } from 'reactstrap';
+import { NavHashLink as NavLink } from 'react-router-hash-link';
+// import projects from '../projects.json'
 
 export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      dropdownOpen: false
-    };
-  }
 
-  toggle() {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen
-    });
-}
+  
 render() {
   return (
     <Fragment>
-      <header id="home">
-        <Nav pills className="navbar navbar-dark bg-dark sticky-top navbar-fixed-top">
-              <NavItem>
-                <h1 className="navbar-brand nav-item wow flip" activeClassName="active" data-wow-delay="7s" data-wow-duration="2s">
-              <a href="#about" class="nave-link" data-scroll="#about" tag={RRNavLink}>About</a> 
-                </h1>
-               </NavItem>
-              <NavItem>
-                <h1 className="navbar-brand wow flip" activeClassName="active" data-wow-delay="7s" data-wow-duration="2s">
-              <a href="#contact" data-scroll="#contact" tag={RRNavLink}>Contact</a>
-                </h1>
-              </NavItem>
-              <NavItem>
-                <NavLink exact to="/" activeClassName="active" tag={RRNavLink}>Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink exact to="/about" activeClassName="active" tag={RRNavLink}>About</NavLink>
-              </NavItem>
-              <NavItem>
-                <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} >
-                  <DropdownToggle nav caret>
-                    Projects
-                  </DropdownToggle>
-                    <DropdownMenu>
-                    <DropdownItem><RRNavLink exact to="/projects"> All Projects</RRNavLink></DropdownItem>
-                    {projects.map((item, i) => (
-                      <DropdownItem key={i}><RRNavLink exact to={`/projects/${i}`}>{item.title}</RRNavLink></DropdownItem>
-                    ))}
-                  </DropdownMenu>
-                </Dropdown>
-              </NavItem>
-              <NavItem>
-                <NavLink exact to="/contact" activeClassName="active" tag={RRNavLink}>Contact</NavLink>
-              </NavItem>
+      <header id="main-header">
+        <Nav className="nav nav-pills">
+          <NavItem>
+            <NavLink
+              className="nav-link"
+              to="/#home"
+              >Home
+           </NavLink>
+            </NavItem>
+          <NavItem>
+            <NavLink
+              className="nav-link"
+              to="/#about"
+              >about
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className="nav-link"
+              to="/#contact"
+              >Contact
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className="nav-link"
+              to="/#projects"
+              >Projects
+          </NavLink>
+          </NavItem>
         </Nav>
       </header>
     </Fragment>
