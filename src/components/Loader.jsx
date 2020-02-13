65bd573e83fa05d79383f6f9bdf720ed0ebd0c6e
ZakMos <z.zza@hotmail.com>
2020-02-13 22:41:48 +0100

    update loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
-const Loader = (props) => (<p>Loading......................... :) </p>);
+const Loader = (props) => (
+                            <div className="lds-roller">
+                                <div>
+                                </div>
+                                    <div>
+                                    </div>
+                                        <div>
+                                        </div>
+                                            <div>
+                                            </div>
+                                                <div>
+                                                </div>
+                                                    <div>
+                                                    </div>
+                                                        <div>
+                                                        </div>
+                                                            <div>
+                                                            </div>
+                            </div>
+                            );
 
 export default Loader;
