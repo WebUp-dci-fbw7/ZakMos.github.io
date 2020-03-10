f71346dda32ca744f8e673548eec4135cc86b607
ZakMos <z.zza@hotmail.com>
2020-03-10 15:18:08 +0100

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 const Footer = (props) => (
   <footer>
     <p>&copy;2019 Zakaria Moslim</p>
+    <div id='go-top'>
+      <a className='smoothscroll' title='Back to Top' href='#home'>
+        <i className='icon-up-open' />
+      </a>
+    </div>
   </footer>
 );
 
