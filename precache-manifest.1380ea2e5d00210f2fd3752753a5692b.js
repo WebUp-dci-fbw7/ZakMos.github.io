de28e22501bc408f884a3e878257b8ec3e04cc32
zakmos <z.zza@hotmail.com>
2020-09-09 09:19:55 +0200

    Updates

---
+++ precache-manifest.1380ea2e5d00210f2fd3752753a5692b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745dadf3e00136106a50cf66645c175e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "324c693fdbb8fd54ed74",
+    "url": "/static/css/2.fb6184f5.chunk.css"
+  },
+  {
+    "revision": "fb9b79287d08780bcd61",
+    "url": "/static/css/main.5b22636c.chunk.css"
+  },
+  {
+    "revision": "324c693fdbb8fd54ed74",
+    "url": "/static/js/2.b8281a3f.chunk.js"
+  },
+  {
+    "revision": "513f64545f0a4c295ec73b44c9a71c8f",
+    "url": "/static/js/2.b8281a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb9b79287d08780bcd61",
+    "url": "/static/js/main.60edbd5a.chunk.js"
+  },
+  {
+    "revision": "526b4a1573d0d7e312b1",
+    "url": "/static/js/runtime-main.9b265073.js"
+  },
+  {
+    "revision": "39b683ad980b2dd6f64ed0bde939bdbb",
+    "url": "/static/media/NewProject.39b683ad.jpg"
+  },
+  {
+    "revision": "bbd3071fd22eb03859a4da89fe1af4ea",
+    "url": "/static/media/arrow-right.bbd3071f.png"
+  },
+  {
+    "revision": "e3d629ea5923976d3c0bdb34a6ccd9f2",
+    "url": "/static/media/dci03.e3d629ea.jpg"
+  },
+  {
+    "revision": "7128843504e3c2ba063678c76eaf2ad3",
+    "url": "/static/media/dci05.71288435.jpg"
+  },
+  {
+    "revision": "2c39efa0dbbd5b9b431831b367623d6a",
+    "url": "/static/media/zakaria-pic.2c39efa0.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32fca48af956e6518cdc2faae9a1eb2c.js"
+  "/precache-manifest.1380ea2e5d00210f2fd3752753a5692b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
