2ea1faba2df7984cdc83b4674f42cfa3f7ea57cc
ZakMos <z.zza@hotmail.com>
2020-09-09 07:01:37 +0200

    added website counter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Footer = (props) => (
   <footer >
-    <div className="footer">
+    <div id="sfcbhjz6y3x2kezf63s7lrxx1jkmj3xqenn">
+    </div>
+      <script type="text/javascript" src="https://counter11.stat.ovh/private/counter.js?c=bhjz6y3x2kezf63s7lrxx1jkmj3xqenn&down=async" async>
+      </script>
+        <a href="https://www.freecounterstat.com">website counter</a>
+        <a href="https://www.freecounterstat.com" title="website counter">
+          <img src="https://counter11.stat.ovh/private/freecounterstat.php?c=bhjz6y3x2kezf63s7lrxx1jkmj3xqenn" border="0" title="website counter" alt="website counter"/>
+        </a>
+        <div className="footer">
       <p>&copy; 2020 Zakaria Moslim</p>
     </div>
   </footer>
