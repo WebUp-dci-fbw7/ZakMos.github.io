75b40812875bd436a4c88852b9711fea7e64b09f
zakmos <z.zza@hotmail.com>
2021-03-12 14:20:40 +0100

    fix bugs, header, split scss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,46 +1,87 @@
-import React, {  Fragment } from 'react';
-import { Nav, NavItem } from 'reactstrap';
-import { NavHashLink as NavLink } from 'react-router-hash-link';
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
 
-export default class Header extends React.Component {
+const Header = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-render() {
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <Fragment>
-      <div id="home"></div>
-      <header id="main-header">
-        <Nav className="nav">
-          <NavItem>
-            <NavLink
-              className="nav-link header-home-nav"
-              to="/#home"
-              >Home
-           </NavLink>
+    <>
+    <div id="home"></div>
+      <div id="main-header" >
+      <Navbar dark  expand="md">
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink className="nav-link focus-in me-auto" href="/#home">Home</NavLink>
             </NavItem>
-          <NavItem>
-            <NavLink
-              className="nav-link"
-              to="/#about"
-              >About
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className="nav-link"
-              to="/#projects"
-            >Projects
-          </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className="nav-link"
-              to="/#contact"
-              >Contact
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </header>
-    </Fragment>
-    );
-  }
+            </Nav>
+            <Nav className="nav-link focus-in me-auto" navbar>
+            <NavItem>
+              <NavLink href="/#about focus-in">About</NavLink>
+            </NavItem>
+            </Nav>
+            <Nav className="nav-link focus-in me-auto" navbar>
+            <NavItem>
+              <NavLink href="/#projects">Projects</NavLink>
+            </NavItem>
+            </Nav>
+            <Nav className="nav-link focus-in me-auto" navbar>
+            <NavItem>
+              <NavLink href="/#contact">Contact</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+      </div>
+    </>
+  );
 }
+
+export default Header;
+
+      // <div id="home"></div>
+      // <header id="main-header">
+      //   <Nav className="nav">
+      //     <NavItem>
+      //       <NavLink
+      //         className="nav-link header-home-nav"
+      //         to="/#home"
+      //         >Home
+      //      </NavLink>
+      //       </NavItem>
+      //     <NavItem>
+      //       <NavLink
+      //         className="nav-link"
+      //         to="/#about"
+      //         >About
+      //       </NavLink>
+      //     </NavItem>
+      //     <NavItem>
+      //       <NavLink
+      //         className="nav-link"
+      //         to="/#projects"
+      //       >Projects
+      //     </NavLink>
+      //     </NavItem>
+      //     <NavItem>
+      //       <NavLink
+      //         className="nav-link"
+      //         to="/#contact"
+      //         >Contact
+      //       </NavLink>
+      //     </NavItem>
+      //   </Nav>
+
