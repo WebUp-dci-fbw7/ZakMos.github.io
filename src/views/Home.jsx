d89384bfa6494908159a3bb2b28e6e2fc0e2c2fa
zakmos <z.zza@hotmail.com>
2021-03-14 15:31:48 +0100

    update home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,4 +1,5 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
+import { Container } from 'reactstrap';
 import personalPic from '../images/zakaria-pic.jpg';
 import newProjects from '../images/NewProject.jpg';
 import welcomeMsg from '../images/dci05.jpg';
@@ -11,8 +12,9 @@ import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView, MDBContainer }
 class Home extends Component {
   render() {
     return (
-      <Fragment>
-        <div className="tab-content">
+    <>
+    <div className="background-home-n11">
+      <Container fluid="xxl">
         <MDBContainer>
           <MDBCarousel
             activeItem={1}
@@ -21,15 +23,15 @@ class Home extends Component {
             showIndicators={false}
             className="z-depth-1"
             slide
-          >
+            >
             <MDBCarouselInner>
               <MDBCarouselItem itemId="1">
                 <MDBView>
                   <img
-                    className="home-img d-block w-100"
+                    className="home-img"
                     src={welcomeMsg}
                     alt="Welcome Message"
-                  />
+                    />
                   <h2 className="welcome-message  text-focus-in">
                     Welcome to my Portfolio
                 </h2>
@@ -38,10 +40,10 @@ class Home extends Component {
               <MDBCarouselItem itemId="2">
                 <MDBView>
                   <img
-                    className="home-img d-block w-100"
+                    className="home-img"
                     src={newProjects}
                     alt="New Projects"
-                  />
+                    />
                 </MDBView>
                 <h2 className="welcome-message text-focus-in">
                   New Projects Coming Soon
@@ -50,10 +52,10 @@ class Home extends Component {
               <MDBCarouselItem itemId="3">
                 <MDBView>
                   <img
-                    className="home-img d-block w-100"
+                    className="home-img"
                     src={niceDay}
                     alt="Third slide"
-                  />
+                    />
                 </MDBView>
                 <h2 className="welcome-message text-focus-in">
                   Have a nice Day...
@@ -62,39 +64,39 @@ class Home extends Component {
             </MDBCarouselInner>
           </MDBCarousel>
         </MDBContainer>
-          <div className="home-personal">
-            <div>
-              <img className="personal-img rounded-circle border mx-auto d-block"
-                    src={personalPic} alt="Zakaria's-pic" />
-            </div>
-            <div className="personal-description">
-              <h2>Hi, I'm Zakaria..</h2>
-              <p className="lead">I have graduated from the Digital Career Institute (DCI), Berlin, as a full stack Web Developer.</p>
-              <p className="lead">
+      </Container>
+     </div>
 
+      <div className="expIconMain">
+        <img className="personalImg rounded-circle puff-in-center "
+             src={personalPic} alt="Zakaria's-pic" />
+        <FontAwesomeIcon className="expIcon experience-icon-1" icon={faHtml5}     size="5x"></FontAwesomeIcon>
+        <FontAwesomeIcon className="expIcon experience-icon-2" icon={faSass}      size="5x"></FontAwesomeIcon>
+        <FontAwesomeIcon className="expIcon experience-icon-3" icon={faJsSquare}  size="5x"></FontAwesomeIcon>
+        <FontAwesomeIcon className="expIcon experience-icon-4" icon={faReact}     size="5x"></FontAwesomeIcon>
+        <FontAwesomeIcon className="expIcon experience-icon-5" icon={faNode}      size="5x"></FontAwesomeIcon>
+        <FontAwesomeIcon className="expIcon experience-icon-6" icon={faPhp}       size="5x"></FontAwesomeIcon>
+        <FontAwesomeIcon className="expIcon experience-icon-7" icon={faAngular}   size="5x"></FontAwesomeIcon>
+      </div>
 
+      <div className="home-personal">
+        <div className="personaDescription background-home-n11">
+          <h2>Hi, I'm Zakaria..</h2>
+            <p className="lead">
+              I have graduated from the Digital Career Institute (DCI), Berlin, as a full stack Web Developer.
+            </p>
+            <p className="lead">
               I have worked on several projects involving HTML5, CSS3, JavaScript, JQuery, React, Redux, NodeJS, express, MongoDB, MySQL, AJAX, Testing Version Control(Git). I have also studied PHP - Laravel, Phaser3, Angular ad Java, Currently I am looking for new challenges as Junior Full Stack Web Developer, and I have an evening Java course at ReDI School.
-              </p>
-              <p className="lead">
-              I have more than 16 years experience in IT, Marketing, PR and office fields as Technical coordinator.</p>
-              <p className="lead">
-              Creativity, motivation, adopting quickly with job or new environment and learning quickly.</p>
-              <p className="lead">
-              Take a look to my old <a href="http://zak.epizy.com/" target="_blank"  rel="noopener noreferrer">Portfolio</a></p>
-            </div>
-          </div>
-          <div className="experience-icons">
-            <FontAwesomeIcon className="experience-icon" icon={faHtml5} size="1x"></FontAwesomeIcon>
-            <FontAwesomeIcon className="experience-icon" icon={faSass} size="1x"></FontAwesomeIcon>
-            <FontAwesomeIcon className="experience-icon" icon={faJsSquare} size="1x"></FontAwesomeIcon>
-            <FontAwesomeIcon className="experience-icon" icon={faReact} size="1x"></FontAwesomeIcon>
-            <FontAwesomeIcon className="experience-icon" icon={faNode} size="1x"></FontAwesomeIcon>
-            <FontAwesomeIcon className="experience-icon" icon={faPhp} size="1x"></FontAwesomeIcon>
-            <FontAwesomeIcon className="experience-icon" icon={faAngular} size="1x"></FontAwesomeIcon>
-          </div>
+            </p>
+            <p className="lead">
+              I have more than 16 years experience in IT, Marketing, PR and office fields as Technical coordinator.
+            </p>
+            <p className="lead">
+              Creativity, motivation, adopting quickly with job or new environment and learning quickly.
+            </p>
         </div>
-
-      </Fragment>
+      </div>
+    </>
   );
   }
 }
