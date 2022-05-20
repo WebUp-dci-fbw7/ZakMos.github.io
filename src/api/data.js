8a425191c809ba5d494987e655922d6cf974a2c1
zakmos <z.zza@hotmail.com>
2022-05-20 09:42:16 +0200

    added data api

---
+++ src/api/data.js
@@ -0,0 +1,82 @@
+const data = [
+	{	id: 1,
+        title:"Corona Tracker",
+		description:"Corona Tracker App",
+		image:"/images/covid-19.png",
+		url: "https://co19.netlify.app/",
+		github: "https://github.com/ZakMos/corona-tracker",
+		tags:["jsx","ReactJs", "Redux", "Javascript", "sass", "bootstrap"]
+	},
+	{
+		id: 2,
+        title: "Mybrary",
+		description: "Library App, view, add, edit delete Authors, Books, using nodejs express mongodb",
+		image: "/images/mybrary.png",
+		url: "https://node-zak-test.herokuapp.com/",
+		github: "https://github.com/ZakMos/node-test",
+		tags: ["NodeJs", "express", "MongoDb", "ejs", "bootstrap"]
+	},
+	{ 
+		id: 3,
+        title:"Shopping Cart",
+		description:"A shopping cart allows a website’s visitor to view product pages and add items to their basket",
+		image:"/images/ShoppingCart.jpg",
+		url: "zak-shoppingcart.netlify.app/",
+		github: "https://github.com/ZakMos/shopping-cart",
+		tags:["jsx","ReactJs", "Redux", "Javascript", "sass", "bootstrap"]
+	},
+	{
+		id: 4,
+        title: "ToDo App",
+		description: "Simple App Todo List Application",
+		image: "/images/Todo.jpg",
+		url: "https://todo-live.netlify.app/",
+		github: "https://github.com/ZakMos/todo-live",
+		tags: ["jsx", "css", "Javascript", "React", "Redux"]
+	},
+	{
+		id: 5,
+        title: "Markdown Blog",
+		description: "Simple Markdown blog website",
+		image: "/images/blog.jpg",
+		url: "https://zak-markdown-blog.herokuapp.com/",
+		github: "https://github.com/ZakMos/markdown-blog",
+		tags: ["ejs", "css", "Bootstrap", "NodeJs", "Express", "MongoDb"]
+	},
+	{	id: 6,
+        title:"I Lost My Runner",
+		description:"A funny Running Man Game using Phaser 3",
+		image:"/images/I-lost-my-runner.jpg",
+		url: "https://ilostmyrunner.netlify.app/",
+		github: "https://github.com/ZakMos/I-lost-my-runners1",
+		tags:["Phaser 3","Javascript","css","html"]
+	},
+	{
+		id: 7,
+        title: "Authentication",
+		description: "Authentication Websites using nodejs express mongodb passport",
+		image: "/images/node_passport_Login.png",
+		url: "https://node-pass.herokuapp.com/",
+		github: "https://github.com/ZakMos/node_passport_login",
+		tags: ["NodeJs", "express", "MongoDb", "ejs", "Passport", "Bcryptjs"]
+	},
+	{
+		id: 8,
+        title: "Mobile Shope",
+		description: "Mobile repair shope, New project still under construction  ",
+		image: "/images/handyHome.png",
+		url: "https://handyhomes.de/",
+		github: "https://github.com/ZakMos/handy-home",
+		tags: ["ReactJs", "jsx", "css"]
+	},
+	{	id: 9,
+        title:"Quizzards",
+		description:"A small quiz app",
+		image:"/images/Quizzard.jpg",
+		url:"https://quizzards.netlify.app/",
+		github: "https://github.com/ZakMos/quizzards",
+		tags:["OOP","javascript","bootstrap","sass","css","html"]
+	}
+]
+
+export default data;
