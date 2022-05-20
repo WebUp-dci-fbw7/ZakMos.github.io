8eb46dcddb4c6f52b67fca0a82eeeab724fecef7
zakmos <z.zza@hotmail.com>
2022-05-20 10:02:37 +0200

    fix bugs

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ import {IoLogoTwitter} from 'react-icons/io';
 const Footer = () => {
   return (
     <footer>
-      <a href="/#" className="footer_logo">zak</a>
+      <a href="/#" className="footer_logo">Zakaria Molsim</a>
       <ul className="permalinks">
         <li><a href="/#">Home</a></li>
         <li><a href="#about">About</a></li>
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -10,7 +10,7 @@ const HeaderSocials = () => {
       <a href="https://www.linkedin.com/in/zakmos" rel="noreferrer" target="_blank"><BsLinkedin/></a>
       <a href="https://codepen.io/ZakMos" rel="noreferrer" target="_blank"><AiOutlineCodepenCircle /></a>
       <a href="https://github.com/ZakMos" rel="noreferrer" target="_blank"><BsGithub /></a>
-      <a href="https://www.instagram.com/zaa_aak/" rel="noreferrer" target="_blank"><BsInstagram /> </a>
+      <a href="https://www.instagram.com/zaa_aak/" rel="noreferrer" target="_blank"><BsInstagram /></a>
     </div>
   )
 }
