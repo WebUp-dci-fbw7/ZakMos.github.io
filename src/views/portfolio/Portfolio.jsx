2fa6f090ae36f211fba3f6974157f73633f96bfc
zakmos <z.zza@hotmail.com>
2022-05-20 09:41:31 +0200

    added portfolio with styling

--- src/views/portfolio/Portfolio.jsx
+++ src/views/portfolio/Portfolio.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import P1 from "../../assets/zakaria.png"
+import data from "../../api/data";
+
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio_container">
+        {
+          data.map(({id, image, title}) => {
+            return (
+              <article key={id} className="portfolio_item">
+                <div className="portfolio_item-image">
+                  <img src={P1} alt="p11" />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio_item-cta">
+                  <a href="www.google.com" className="btn" target="_blank">Github</a>
+                  <a href="www.google.com" className="btn btn-primary">Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
